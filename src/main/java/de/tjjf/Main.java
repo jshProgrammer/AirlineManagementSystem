e5b658151fdb5002c1945a759050e5dc8da648ca
xjasx4 <jasmin.wander04@gmail.com>
2024-11-02 12:22:50 +0100

    Initializing project with maven, hibernate and h2

--- src/main/java/de/tjjf/Main.java
+++ src/main/java/de/tjjf/Main.java
@@ -34,7 +34,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Jasmin Test");
+        System.out.println("JasminTest");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
         EntityManager em = emf.createEntityManager();
