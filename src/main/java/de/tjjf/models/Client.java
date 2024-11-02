3e7a5874f9215ee54c6df9ed5d217f1ef9b53ab4
tomknoblach <tomknoblach@t-online.de>
2024-11-02 13:02:21 +0100

    Update Name

--- src/main/java/de/tjjf/models/Client.java
+++ src/main/java/de/tjjf/models/Client.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class client {
+public class Client {
     @Id
     private int personID;
     private boolean isBusinessClient;
