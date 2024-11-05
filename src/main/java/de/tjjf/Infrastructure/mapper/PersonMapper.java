822e60220b169ddc8f3bbcef18e58da4277d0604
tomknoblach <tomknoblach@t-online.de>
2024-11-05 19:46:48 +0100

    Implementing of PersonMapper

---
+++ src/main/java/de/tjjf/Infrastructure/mapper/PersonMapper.java
@@ -0,0 +1,35 @@
+package de.tjjf.Infrastructure.mapper;
+
+import de.tjjf.Domain.models.MPerson;
+import de.tjjf.Infrastructure.models.Person;
+
+public class PersonMapper {
+
+    public static Person toEntity(MPerson mPerson){
+        return new Person(
+                mPerson.getPersonId(),
+                mPerson.getFirstName(),
+                mPerson.getMiddleNames(),
+                mPerson.getLastName(),
+                mPerson.getDateOfBirth(),
+                mPerson.getPhonenumber(),
+                mPerson.getAddress(),
+                mPerson.getEmail(),
+                mPerson.getPassword()
+        );
+    }
+
+    public static MPerson toDomain(Person person){
+        return new MPerson(
+                person.getPersonId(),
+                person.getFirstName(),
+                person.getMiddleNames(),
+                person.getLastName(),
+                person.getDateOfBirth(),
+                person.getPhonenumber(),
+                person.getAddress(),
+                person.getEmail(),
+                person.getPassword()
+        );
+    }
+}
--- src/main/java/de/tjjf/Infrastructure/models/Person.java
+++ src/main/java/de/tjjf/Infrastructure/models/Person.java
@@ -16,6 +16,7 @@ public class Person
     @Column(nullable = false)
     private String firstName;
 
+    //evtl nullable true, nicht jeder hat middle name
     @Column(nullable = false)
     private String middleNames;
 
@@ -52,6 +53,78 @@ public class Person
         this.phonenumber = phonenumber;
         this.address = address;
         this.email = email;
+        this.password = password;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleNames() {
+        return middleNames;
+    }
+
+    public void setMiddleNames(String middleNames) {
+        this.middleNames = middleNames;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
