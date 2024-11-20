f99ff367092401ef2430cf52f8624227df3465c0
hendrickson187 <finnkrappitz@web.de>
2024-11-20 09:34:29 +0100

    sendEmailTest.java, pom.xml Anpassung (com.sun.mail)

---
+++ src/test/java/de/tjjf/CRUDTests/sendEmailTest.java
@@ -0,0 +1,17 @@
+package de.tjjf.CRUDTests;
+
+import de.tjjf.Domain.EmailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class sendEmailTest {
+
+    @Test
+    public void testEmailSentSuccessfully() {
+        EmailSender emailSender = new EmailSender();
+        emailSender.sendMail("jpfennig2403@gmail.com", "sendEmailTestCase", "TestContent");
+            System.out.println("E-Mail wurde erfolgreich gesendet");
+            assertTrue(true, "E-Mail wurde erfolgreich gesendet.");
+    }
+}
