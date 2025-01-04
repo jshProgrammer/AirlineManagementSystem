ea51a7a0f0b0a0a3224e3bcc3dda6e659294d1b4
tomknoblach <tomknoblach@t-online.de>
2025-01-04 16:18:19 +0100

    Payment is working now, but has to be checked and cleaned up a bit. FlightMapper is Missing or is now requiered(who knows) only created class for compiling but isn't filled, hadn`t much time, sorry

---
+++ src/main/java/de/tjjf/Infrastructure/api/MapperInput/FlightMapperInput.java
@@ -0,0 +1,12 @@
+package de.tjjf.Infrastructure.api.MapperInput;
+
+import de.tjjf.Infrastructure.api.InputModels.APIFlightInput;
+import de.tjjf.Infrastructure.api.models.APIFlight;
+
+public class FlightMapperInput {
+    public APIFlight toDomain(Long a, APIFlightInput input){
+        return new APIFlight(
+
+        );
+    }
+}
