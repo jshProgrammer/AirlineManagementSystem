57ead49a878d44b871c4611e656e0b20b0b8b6bd
tomknoblach <tomknoblach@t-online.de>
2025-01-06 08:14:47 +0100

    (with Joshua and Jasmin) cleaning payment

--- src/main/java/de/tjjf/Domain/UseCases/PaymentUseCase.java
+++ src/main/java/de/tjjf/Domain/UseCases/PaymentUseCase.java
@@ -27,17 +27,6 @@ public class PaymentUseCase extends AuthorizedUseCase {
         super(AuthenticationUseCase.getInstance());
     }
 
-    /*public static boolean paymentCall(int amountEuros, MPayment mPayment) {
-        String method = "";
-        try {
-            method = createPaymentMethodFromMPayment(mPayment);
-        } catch (StripeException e) {
-            e.printStackTrace();
-            return false;
-        }
-        //return paymentImpl(amountEuros, method);
-        return paymentImpl(amountEuros, mPayment);
-    }*/
     static{
         try {
             Stripe.apiKey = PasswordDecryption.decryptPassword("src/main/java/de/tjjf/Domain/paymentApiKey.enc", "src/main/java/de/tjjf/Domain/paymentApiKey.key");
@@ -79,35 +68,6 @@ public class PaymentUseCase extends AuthorizedUseCase {
         }
     }
 
-    /*public static PaymentIntent paymenthope(String tokenId, long amount){
-        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
-                .setAmount(amount)
-                .setCurrency("eur")
-                .setPaymentMethod(tokenId) // Direkt den Test-Token verwenden
-                .setConfirm(true)
-                .addPaymentMethodType(Collections.singletonList("card")) // Kartenzahlung
-                .build();
-
-        try {
-            return PaymentIntent.create(params);
-        } catch (StripeException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static String createPaymentMethod(String tokenId) {
-        PaymentMethodCreateParams params = PaymentMethodCreateParams.builder()
-                .setType(PaymentMethodCreateParams.Type.CARD)
-                .setCard(PaymentMethodCreateParams.CardDetails.builder()
-                        .setToken)
-                .build();
-
-        try {
-            PaymentMethod paymentMethod = PaymentMethod.create(params);
-            return paymentMethod.getId(); // Gibt die PaymentMethod-ID zurück
-        } catch (StripeException e) {
-            throw new RuntimeException("Fehler beim Erstellen der PaymentMethod", e);
-        }
-    }*/
 
 
     public static boolean paymentImpl(int amountEuros, MPayment method) {
@@ -161,89 +121,6 @@ public class PaymentUseCase extends AuthorizedUseCase {
         }
 
     }
-    /*public static boolean paymentImpl(int amountEuros, String paymentMethodId) {
-        try {
-            Stripe.apiKey = PasswordDecryption.decryptPassword(
-                    "src/main/java/de/tjjf/Domain/paymentApiKey.enc",
-                    "src/main/java/de/tjjf/Domain/paymentApiKey.key"
-            );
-
-            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
-                    .setAmount((long) (amountEuros * 100))
-                    .setCurrency("eur")
-                    .setPaymentMethod(paymentMethodId)
-                    .addPaymentMethodType("card")
-                    .setConfirm(true)
-                    .build();
-
-            PaymentIntent intent = PaymentIntent.create(params);
-            return intent != null && "succeeded".equals(intent.getStatus());
-
-        } catch (StripeException e) {
-            e.printStackTrace();
-            return false;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    /*public static boolean paymentImpl(int amountEuros, String paymentMethodId) {
-        try {
-            // Setze den Stripe-API-Key (Test-API-Key sicherstellen)
-            Stripe.apiKey = PasswordDecryption.decryptPassword(
-                    "src/main/java/de/tjjf/Domain/paymentApiKey.enc",
-                    "src/main/java/de/tjjf/Domain/paymentApiKey.key"
-            );
-        } catch (Exception e) {
-            throw new RuntimeException("Password could not be decrypted");
-        }
-        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
-                .setAmount((long)(amountEuros * 100))
-                .setCurrency("eur")
-                .setAutomaticPaymentMethods(
-                        PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
-                                .setEnabled(true)
-                                .build())
-                // .setConfirmationMethod(PaymentIntentCreateParams.ConfirmationMethod.MANUAL) // Entferne das hier, wenn du `automatic_payment_methods` verwendest
-                .build();
-        //PaymentIntent paymentIntent = PaymentIntent.create(params);
-
-        // Parameter für PaymentIntent erstellen
-        /*Map<String, Object> params = new HashMap<>();
-        params.put("amount", amountEuros * 100); // Betrag in Cent
-        params.put("currency", "eur");
-        params.put("payment_method", paymentMethodId);
-        params.put("confirmation_method", "manual");
-        params.put("confirm", true);
-        .setAutomaticPaymentMethods(
-                        PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
-                                .setEnabled(true)
-                                .build()
-                )*/
-
-
-        // Automatische Zahlungsmethoden aktivieren (keine Redirects)
-    // Map<String, Object> autoPaymentMethods = new HashMap<>();
-       /* autoPaymentMethods.put("enabled", true);
-        autoPaymentMethods.put("allow_redirects", false);
-        //params.put("automatic_payment_methods", autoPaymentMethods);
-
-        try {
-            // PaymentIntent erstellen
-            PaymentIntent intent = PaymentIntent.create(params);
-            if ("succeeded".equals(intent.getStatus())) {
-                System.out.println("Payment successful");
-                return true;
-            } else {
-                System.out.println("Payment failed with status: " + intent.getStatus());
-                return false;
-            }
-        } catch (StripeException e) {
-            System.out.println("Payment failed due to an error:");
-            e.printStackTrace();
-            return false;
-        }
-    }*/
 
     // Methode zur Konvertierung von MPayment zu einer PaymentMethod
     public static String createPaymentMethodFromMPayment(MPayment mPayment) throws StripeException {
@@ -291,42 +168,6 @@ public class PaymentUseCase extends AuthorizedUseCase {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-    }
-    public String getClientSecret() {
-        SetupIntent setupIntent = createSetupIntent();
-        if (setupIntent != null) {
-            return setupIntent.getClientSecret();  // Gib das client_secret zurück
-        }
-        return null;
-    }
-
-    public boolean createPaymentIntent(String paymentMethodId, int amount) {
-        try {
-            Stripe.apiKey = PasswordDecryption.decryptPassword("src/main/java/de/tjjf/Domain/paymentApiKey.enc", "src/main/java/de/tjjf/Domain/paymentApiKey.key");  // Ersetze mit deinem Secret Key
-
-            // Erstelle den PaymentIntent
-            PaymentIntentCreateParams params =
-                    PaymentIntentCreateParams.builder()
-                            .setAmount((long) (amount * 100)) // Betrag in kleinster Einheit (z. B. Cent)
-                            .setCurrency("eur")
-                            .setAutomaticPaymentMethods(
-                                    PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
-                                            .setEnabled(true)
-                                            .build()
-                            )
-                            .build();
-
-            PaymentIntent intent = PaymentIntent.create(params);
-
-            if ("succeeded".equals(intent.getStatus())) {
-                return true;  // Zahlung erfolgreich
-            } else {
-                return false;  // Zahlung fehlgeschlagen
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
         }
     }
 }
