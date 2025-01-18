9857c9cedb0a0798a719cc9f62a32dc0ea783225
jshProgrammer <jpfennig2403@gmail.com>
2025-01-18 11:27:14 +0100

    (with Finn and Tom) correcting implementation of CancelCompleteFlight-functionality via API

--- src/main/java/de/tjjf/Domain/UseCases/CancelCompleteFlightUseCase.java
+++ src/main/java/de/tjjf/Domain/UseCases/CancelCompleteFlightUseCase.java
@@ -2,11 +2,6 @@ package de.tjjf.Domain.UseCases;
 
 import de.tjjf.Domain.EmailSender;
 import de.tjjf.Domain.models.MFlight;
-import de.tjjf.Infrastructure.Client.ClientOperations.APIOperations.FlightAPIOperation;
-import de.tjjf.Infrastructure.api.InputModels.APIFlightInput;
-import de.tjjf.Infrastructure.api.MapperInput.FlightMapperInput;
-import de.tjjf.Infrastructure.api.mapper.APIFlightMapper;
-import de.tjjf.Infrastructure.api.models.APIFlight;
 import de.tjjf.Infrastructure.persistence.DBOperations.ImplOperations.Update.FlightUpdateImpl;
 import de.tjjf.Infrastructure.persistence.mapper.FlightMapper;
 
